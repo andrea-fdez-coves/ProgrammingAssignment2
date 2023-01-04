427658461ceb38c84d34ebe49d966b4c2da298a3
andrea-fdez-coves <andreafecoves@gmail.com>
2023-01-04 12:27:33 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
+## Week 3 of the R Programming course from Coursera (r)
 
-## Write a short comment describing this function
+## Original code:rdpeng (Roger D. Peng, Professor of Statistics and Data 
+## Sciences at the University of Texas, Austin) 
+## Edited by: Andrea Fernández Coves on Jan 2023
 
+
+## In this assignment, we need write a pair of functions that catche the 
+## inverse of a matrix
+
+#makeCacheMatrix creates a special "matrix" object that can cahche its inverse
 makeCacheMatrix <- function(x = matrix()) {
+        s <- NULL
+        #we will use the solve function to calculate the inverse
+        set <- function(y){
+                x <<-y
+                s <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inv) s<<-inv
+        getInv <- function() s
+        list(set=set, get=get, setInv=setInv, getInv=getInv)
 
 }
 
 
-## Write a short comment describing this function
+#cacheSolve computes the inverse of the special matrix returned by the function
+#"makeCacheMatrix" (above). If the inverse was already calculated (i.e., the
+# matrix has not changed), then this function should retrieve the inverse from
+# the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # The first thing will do is to check if the matrix has been inversed,
+        #if so, we will get the inverse (Result fros solve) from the cache and
+        # skip the computation
+        s <- x$getInv()
+        if (!is.null(s)){
+                message ("getting chaged data")
+                return(s)
+        }
+        dataInv <- x$get()
+        s <- solve(dataInv, ...)
+        x$setInv(s)
+        ## Return a matrix that is the inverse of 'x', in this case, s
+        s
 }
